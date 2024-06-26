ea3633a2ffe66e4ca80cdcbb6a0c056fa2cd8676
Aritro123 <itsaritro990@gmail.com>
2024-06-27 03:46:46 +0530

    Set ORIGIN for deploy

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN,
     credentials: true,
   },
 });
