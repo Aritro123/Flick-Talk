238f437721287f5ad7cc5333997c0f08773af53d
Aritro123 <itsaritro990@gmail.com>
2024-07-28 22:55:03 +0530

    Change in frontend route

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://flick-talk.onrender.com";
+export const host = "https://flick-talk.netlify.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar/`;
